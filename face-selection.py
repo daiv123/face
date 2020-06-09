ddf16f5b218de0bcfc054a1571113ea9274c8403
Davin Clark <davinic2@illinois.edu>
2020-06-08 20:34:05 -0700

    finished test model with three faces

--- face-selection.py
+++ face-selection.py
@@ -4,22 +4,30 @@ import cv2
 
 inDir = sys.argv[1]
 outDir = sys.argv[2]
+if len(sys.argv) > 3:
+    i = int(sys.argv[3])
+else :
+    i = 0
 
 
 faceCascade = cv2.CascadeClassifier('/Users/davinclark/Downloads/haarcascade_frontalface_alt.xml')
 faceString = "face{}.png"
-for entry in os.scandir(inDir):
-    if(entry.is_file() and entry.name[0]!="."):
-        img = cv2.imread(entry.name)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        faces = faceCascade.detectMultiScale(gray,scaleFactor=1.5)
-        os.chdir(outDir)
-        for i, (x,y,w,h) in enumerate(faces):
-            imgCopy = cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            cv2.imshow("press k to keep",imgCopy)
-            key = 0xFF & cv2.waitKey()
-            if key == ord('k'):
-                cv2.imsave(faceString.format(i))
-            cv2.destroyAllWindows()
+images = [cv2.imread(file.path) for file in os.scandir(inDir) if (file.is_file() and file.name[0] != '.')]
+
+os.chdir(outDir)
+for img in images:
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(gray,scaleFactor=1.1)
+    for (x,y,w,h) in faces:
+        imgCopy = img.imgCopy()
+        imgCopy = cv2.rectangle(imgCopy, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.imshow("press k to keep",imgCopy)
+        key = 0xFF & cv2.waitKey()
+        if key == ord('k'):
+            cv2.imwrite(faceString.format(i), img[y:y+h, x:x+w])
+            i+=1
+        cv2.destroyAllWindows()
+
     
             
